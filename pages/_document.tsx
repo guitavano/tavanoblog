5b1d2421478f799eda6c730c2696970ca5d71f85
guilhermetavanorocky <guilherme.santos@rocky.ag>
2022-05-16 17:48:25 -0300

    add gsc tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,8 @@ export default class MyDocument extends Document {
             <Html>
                 <Head>
                     <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
+                    
+                    <meta name="google-site-verification" content="xKxIrkDJaQqe7DOYQG4sDCw4P4y98BS8OgucZzL4CFs" />
                 </Head>
                 <body>
                     <Main />
