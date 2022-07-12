7c4f18e82206732680c87781c05005f3ca32dc5d
guilhermetavanorocky <guilherme.santos@rocky.ag>
2022-07-11 21:44:23 -0300

    add lang attribute

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 export default class MyDocument extends Document {
     render() {
         return (
-            <Html>
+            <Html lang='pt-BR'>
                 <Head>
                     <link rel="shortcut icon" href="./../TavanoBlogIcon.ico" type="image/x-icon" />
                     
