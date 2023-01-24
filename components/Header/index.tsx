f72c48d5971738564576962368113f4398fef9a1
guitavano <tavano62@gmail.com>
2023-01-23 22:48:26 -0300

    change initial state of isSearching

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -7,7 +7,7 @@ import { useRouter } from 'next/router'
 export default function Header() {
 
     const [search, setSearch] = useState("")
-    const [isSearching, setIsSearching] = useState(true)
+    const [isSearching, setIsSearching] = useState(false)
 
     function handleKeyDown (e) {
         if (e.key === 'Enter' && search.length > 0) {
